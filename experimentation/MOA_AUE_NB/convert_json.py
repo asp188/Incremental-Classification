27342b0543210354b31ea532ed482cdd5c4b7f0e
qibaoyuan <qibaoyuan@126.com>
2015-05-12 16:36:42 +0800

    code format and bug fix

--- experimentation/MOA_AUE_NB/convert_json.py
+++ experimentation/MOA_AUE_NB/convert_json.py
@@ -1,29 +1,34 @@
-#COnvert the MOA predicted files to JSON format
-import simplejson as json
+# COnvert the MOA predicted files to JSON format
 import re
 import os
-files=['em.pred','sea.pred','tj.pred']
-path ='experimentation/MOA_AUE_NB/'
+
+import simplejson as json
+import numpy as np
+
+
+files = ['em.pred', 'sea.pred', 'tj.pred']
+path = 'experimentation/MOA_AUE_NB/'
 
 for filename in files:
     dataset = re.search('(.*).pred', filename, re.IGNORECASE)
-    dataset= dataset.group(1)
-    
-    file_path=os.getcwd()+'/'+filename
-    
-    data=np.recfromcsv(file_path)
-    error= [np.abs(data['real'][i]-data['pred'][i]) for i in range(0,len(data))]
+    dataset = dataset.group(1)
 
-    #find chunk accuracy
-    total_accuracy=0
-    chunk_size=1000
+    file_path = os.getcwd() + '/' + filename
 
-    chunk_accuracy_list= [1-np.mean(error[i:i+chunk_size]) for i in range(0,len(data),chunk_size)]
-    overall_accuracy=1-np.mean(error)
+    data = np.recfromcsv(file_path)
+    error = [np.abs(data['real'][i] - data['pred'][i]) for i in range(0, len(data))]
+
+    # find chunk accuracy
+    total_accuracy = 0
+    chunk_size = 1000
+
+    chunk_accuracy_list = [1 - np.mean(error[i:i + chunk_size]) for i in range(0, len(data), chunk_size)]
+    overall_accuracy = 1 - np.mean(error)
 
     #write to JSON file
-    op=[{"dataset":dataset,"chunk_size":chunk_size, "chunk_accuracy":chunk_accuracy_list,  "overall_accuracy":overall_accuracy, "error_list":error}]
-    output_file=os.getcwd()+'/'+dataset+'.dat'
-    f=open(output_file,'w')
-    json.dump(op,f)
+    op = [{"dataset": dataset, "chunk_size": chunk_size, "chunk_accuracy": chunk_accuracy_list,
+           "overall_accuracy": overall_accuracy, "error_list": error}]
+    output_file = os.getcwd() + '/' + dataset + '.dat'
+    f = open(output_file, 'w')
+    json.dump(op, f)
     f.close()
