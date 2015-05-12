8d8536b6031b56b54c205be29eae7566189c94b1
qibaoyuan <qibaoyuan@126.com>
2015-05-12 16:38:54 +0800

    code format and bug fix

--- scripts/scrap/scrap1/em_test (copy).py
+++ scripts/scrap/scrap1/em_test (copy).py
@@ -11,22 +11,24 @@ from sklearn.metrics import classification_report, confusion_matrix, accuracy_sc
 from sklearn.linear_model import LogisticRegression
 
 import main_Algo_Geenric as ma
-from plot_running_avg import plot_running_accuracy
+
+
+# from plot_running_avg import plot_running_accuracy
 
 
 # initialize random number generator
 np.random.seed(2000)
 
 filename = '../../dataset/em_dataset/em_dataset.csv'
-#filename ='../../dataset/sea_dataset/normalized_sea.csv'
-#filename ='../../dataset/tj_stream/tj_stream.csv'
+# filename ='../../dataset/sea_dataset/normalized_sea.csv'
+# filename ='../../dataset/tj_stream/tj_stream.csv'
 predicted_labels = []
 testing_label = []
 real_labels = []
 predicted_labels_baseline = []
 
-nop = 100  #em-100
-chunk_size = 10  #em-10
+nop = 100  # em-100
+chunk_size = 10  # em-10
 accuracy_chunking = 1000
 multiplier = int(float(accuracy_chunking) / chunk_size)
 [chunk_accuracy_list, predicted_labels, real_labels] = ma.stub(filename, chunk_size, nop)
@@ -34,14 +36,14 @@ mp.pyplot.close()
 mp.pyplot.figure()
 
 
-#performance computation
+# performance computation
 print(classification_report(real_labels, predicted_labels, target_names=['Class 0', 'Class1']))
 print(confusion_matrix(real_labels, predicted_labels))
 print('Accuracy using PF-CD' + str(accuracy_score(real_labels, predicted_labels)))
 plot_running_accuracy(real_labels, predicted_labels)
 
 
-#performance computation with static model
+# performance computation with static model
 data = np.recfromcsv(filename)
 data_tuplelist = data.tolist()
 training = int(0.1 * len(data_tuplelist))
@@ -67,7 +69,7 @@ mp.pyplot.show()
 
 mp.pyplot.figure()
 
-#Computing chunk wise accuracy
+# Computing chunk wise accuracy
 chunk_accuracy_list_baseline = [np.mod(predicted_labels_baseline[i] + testing_label[i] + 1, 2) for i in
                                 range(0, len(predicted_labels_baseline))]
 ma.plot_accuracy(chunk_accuracy_list, multiplier)
@@ -77,5 +79,5 @@ mp.pyplot.ylabel('Accuracy/Chunk')
 mp.pyplot.title(' Accuracy/Chunk (Baseline vs PF-LR)')
 mp.pyplot.ylim(0, 1)
 mp.pyplot.legend(['PF-LR', 'Baseline'])
-#mp.pyplot.show()
-#ma.plot_accuracy(chunk_accuracy_list,multiplier) 
+# mp.pyplot.show()
+# ma.plot_accuracy(chunk_accuracy_list,multiplier)
